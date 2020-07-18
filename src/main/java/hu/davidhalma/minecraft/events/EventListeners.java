1110f8a15ce43d417fb7c476ec6c45188ccdc6c6
Dávid Halma <halma.david.88@gmail.com>
2020-07-18 22:48:14 +0200

    send login usage message to user while not logged in

--- src/main/java/hu/davidhalma/minecraft/events/EventListeners.java
+++ src/main/java/hu/davidhalma/minecraft/events/EventListeners.java
@@ -1,7 +1,10 @@
 package hu.davidhalma.minecraft.events;
 
+import hu.davidhalma.minecraft.AuthenticationPlugin;
+import hu.davidhalma.minecraft.config.Messages;
 import hu.davidhalma.minecraft.service.Authentication;
 import lombok.extern.log4j.Log4j2;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +14,8 @@ import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.springframework.stereotype.Component;
 
 @Log4j2
@@ -18,14 +23,31 @@ import org.springframework.stereotype.Component;
 public class EventListeners implements Listener {
 
   private final Authentication authentication;
+  private final Messages messages;
 
-  public EventListeners(Authentication authentication) {
+  public EventListeners(Authentication authentication, Messages messages) {
     this.authentication = authentication;
+    this.messages = messages;
   }
 
   @EventHandler
   public void onPlayerJoin(PlayerJoinEvent event) {
-    log.info(event.getPlayer().getDisplayName() + " joined the server...");
+    Player player = event.getPlayer();
+    log.info(player.getDisplayName() + " joined the server...");
+    printScheduledLoginUsage(player);
+  }
+
+  private void printScheduledLoginUsage(Player player) {
+    new BukkitRunnable() {
+      @Override
+      public void run() {
+        if (authentication.loggedIn(player.getUniqueId().toString())){
+          cancel();
+        }
+        player.sendMessage(messages.getWhileUserIsNotLoggedIn());
+      }
+    }.runTaskTimerAsynchronously(JavaPlugin.getProvidingPlugin(AuthenticationPlugin.class), 0, 320);
+
   }
 
   @EventHandler(ignoreCancelled = true)
